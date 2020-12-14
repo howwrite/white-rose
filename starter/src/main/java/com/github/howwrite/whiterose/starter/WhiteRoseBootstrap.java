a1dc1d75183f5e3d541250459a5ff349c652babb
朱森林 <zsl240151@alibaba-inc.com>
2020-12-15 07:28:57 +0800

    init project

---
+++ starter/src/main/java/com/github/howwrite/whiterose/starter/WhiteRoseBootstrap.java
@@ -0,0 +1,15 @@
+package com.github.howwrite.whiterose.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author howwrite
+ * @date 2020/12/15 7:24 上午
+ */
+@SpringBootApplication
+public class WhiteRoseBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteRoseBootstrap.class, args);
+    }
+}
